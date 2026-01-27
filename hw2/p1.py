9c4915423b8004feb42d1aa14c0b6d1a1c95be7d
Ian Fischer <iafi3572@colorado.edu>
2026-01-27 16:12:54 -0700

    p1,2 hw 2

---
+++ hw2/p1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+gamma = 4 
+x0 = 10
+Ns = [100,1000,10000]
+
+x = np.linspace(x0, 60, 1000)
+C = (gamma - 1) * x0**(gamma - 1)
+pdf = C * x**(-gamma)
+
+def sample(N, gamma, x0):
+    u = np.random.rand(N)
+    return x0 * (1 - u)**(-1 / (gamma - 1))
+
+for N in Ns:
+    samples = sample(N, gamma, x0)
+    
+    plt.figure()
+    plt.hist(samples, bins=50, range=(0, 60), density=True)
+    plt.plot(x, pdf)
+    plt.xlim(0, 60)
+    plt.xlabel("x")
+    plt.ylabel("Density")
+    plt.title(f"Power-law samples (N = {N})")
+    plt.show()
---
+++ hw2/p2.py
@@ -0,0 +1,69 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+def newton(u, max_iter=20, tol=1e-10):
+    x = -np.log(1 - u)
+
+    for _ in range(max_iter):
+        g  = 1.0 - (x + 1.0) * np.exp(-x) - u
+        gp = x * np.exp(-x)
+        x_new = x - g / gp
+        if x_new <= 0:
+            x_new = 1e-8
+        if abs(x_new - x) < tol:
+            break
+        x = x_new
+    return x
+
+def partb():
+    N = 10**6
+    U = np.random.rand(N)
+    X = np.empty(N)
+
+    start = time.time()
+
+    for i in range(N):
+        X[i] = newton(U[i])
+
+    end = time.time()
+    print(f"Runtime: {end - start:.2f} seconds")
+    plt.hist(X, bins=100, density=True)
+    x_vals = np.linspace(0, X.max(), 500)
+    pdf_vals = x_vals * np.exp(-x_vals)
+    plt.plot(x_vals, pdf_vals)
+
+    plt.xlabel("x")
+    plt.ylabel("Density")
+    plt.title("Inverse Sampling")
+    plt.show()
+
+
+def partc():
+    N = 10**6
+    c = 4/np.e
+    accepted = np.empty(N)
+    count = 0
+    start = time.time()
+
+    while count < N:
+        X = np.random.exponential(scale=2.0)
+        u = np.random.rand()
+
+        if u <= (X * np.exp(1 - X/2)) / 2:
+            accepted[count] = X
+            count += 1
+
+    end = time.time()
+
+    print(f"Runtime: {end - start:.2f} seconds")
+
+def partd():
+    N = 10**6
+
+    start = time.time()
+    E1 = np.random.exponential(scale=1.0, size=N)
+    E2 = np.random.exponential(scale=1.0, size=N)
+    X = E1 + E2
+    end = time.time()
+    print(f"Runtime: {end - start:.4f} seconds")
+partb()
