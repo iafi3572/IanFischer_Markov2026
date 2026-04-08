b71ed29038a2048cc063257361e86b6c8eb6052c
Ian Fischer <iafi3572@colorado.edu>
2026-04-08 10:26:48 -0600

    a'

---
+++ hw7/p1.py
@@ -0,0 +1,57 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+# when to truncate
+N = 10
+
+def poisson(l):
+    return lambda x: math.exp(-l) *  l**x/math.factorial(x)
+
+# part a
+pX_a = poisson(2) 
+pX_b = poisson(1.5) 
+
+
+pAwins = 0
+for i in range(N+1):
+    subtotal = 0
+    for j in range(i):
+        subtotal += pX_b(j)
+    pAwins += pX_a(i) * subtotal
+
+print(f"Probability Team A wins: {pAwins}")
+
+# part b
+A = lambda t: poisson(2-2/90 * t)
+B = lambda t: poisson(1.5-1.5/90 * t)
+tvals = np.arange(0,91,1)
+pTieVals = []
+
+for t in tvals:
+    pTie = 0
+    for i in range(N+1):
+        pTie += A(t)(i) * B(t)(i)
+    pTieVals.append(pTie)
+
+plt.plot(tvals,pTieVals)
+plt.show()
+
+
+#part c
+pTieVals2 = []
+for t in tvals:
+    pTie = 0
+    if t < 60:
+        for i in range(N+1):
+            pTie += A(t)(i) * B(t)(i)
+    else:
+        for i in range(N+1):
+            pTie += A(t)(i) * B(t)(i+1)
+    
+    pTieVals2.append(pTie)
+
+plt.plot(tvals, pTieVals2)
+plt.show()
+
+
---
+++ hw7/p2.py
@@ -0,0 +1,83 @@
+import numpy as np
+import math 
+import matplotlib.pyplot as plt
+
+l = 3
+
+Finv = lambda u: -1/l *  math.ln(u)
+
+def sim_times():
+    times = []
+    t = 0
+    while t<=48:
+        t += np.random.exponential(1/l)
+        if(t<=48):
+            times.append(t)
+    return np.array(times)
+
+Atimes = sim_times()
+Btimes = sim_times()
+
+plt.eventplot([Atimes, Btimes],
+              colors=['red', 'blue'],
+              lineoffsets=[1, 0],
+              linelengths=0.8)
+
+plt.yticks([0,1], ['Team B', 'Team A'])
+plt.xlim(0, 48)
+plt.xlabel("Time (minutes)")
+plt.title("Basketball Game Simulation (λ = 3 per minute)")
+plt.show()
+
+
+
+def sim_times2():
+    Atimes = []
+    Btimes = []
+    t = 0
+    while t<=48:
+        t += np.random.exponential(1/(2*l))
+        if(t<=48):
+            U = np.random.choice([0,1])
+            if(U):
+                Atimes.append(t)
+            else:
+                Btimes.append(t)
+            
+    return np.array(Atimes), np.array(Btimes)
+
+Atimes,Btimes = sim_times2()
+
+plt.eventplot([Atimes, Btimes],
+              colors=['red', 'blue'],
+              lineoffsets=[1, 0],
+              linelengths=0.8)
+
+plt.yticks([0,1], ['Team B', 'Team A'])
+plt.xlim(0, 48)
+plt.xlabel("Time (minutes)")
+plt.title("Basketball Game Simulation (λ = 3 per minute)")
+plt.show()
+
+
+
+
+num_games = 10**5
+
+N = np.random.poisson(lam = 2*l*48,size=num_games)
+NA = np.random.binomial(n=N, p=0.5)
+NB = N-NA
+D = 2 * (NA-NB)
+
+
+
+E_D = np.mean(D)
+Var_D = np.var(D)
+P_tie = np.mean(D == 0)
+
+print(f"Estimated E[D]: {E_D}, Theorectical E[D]: 0")
+print(f"Estimated Var[D]: {Var_D}, Theorectical Var[D] = {8*l*48}"  )
+print(f"Estimated P(D=0): {P_tie}, Theorectical P(D=0): {1/math.sqrt(math.pi * l * 48)} " )
+
+
+
